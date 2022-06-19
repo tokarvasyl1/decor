94c804db3b9f3e4aa4ab99e745775afbeda93e1b
tokarvasyl1 <tokarvasyl1@gmail.com>
2022-06-19 19:43:48 +0300

    change fonts and add slider

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:54,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:4
+            }
+        }
+    });
+});
+
+
+
+
+
+
+// Тест бургер меню
+// document.querySelector('.hamburger').addEventListener('click', function (e){
+//     e.preventDefault();
+//     // this.classList.toggle('is-active');
+//     if (this.classList.contains('is-active')) {
+//         this.classList.remove('is-active');
+//         document.querySelector('#menu').classList.remove('nav-active');
+//         document.body.classList.remove('body-active');
+//     }
+//     else {
+//         this.classList.add('is-active');
+//         document.querySelector('#menu').classList.add('nav-active');
+//         document.body.classList.add('body-active');
+//     }
+// })
